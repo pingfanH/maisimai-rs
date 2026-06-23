39ff1212a92d85e36f4e90af9822415c53756ca7
Neuron-Group <2062512253@qq.com>
2026-06-23 22:39:09 +0800

    Add flake entrypoint

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "maisimai-rs";
+
+  inputs = {
+    self.submodules = true;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        commonEnv = ''
+          export CARGO="${pkgs.cargo}/bin/cargo"
+          export RUSTC="${pkgs.rustc}/bin/rustc"
+          export CARGO_TARGET_DIR="$PWD/target/nix"
+        '';
+        buildScript = pkgs.writeShellApplication {
+          name = "maisimai-build";
+          runtimeInputs = with pkgs; [ cargo rustc ];
+          text = ''
+            ${commonEnv}
+            if [ "$#" -eq 0 ]; then
+              set -- --lib
+            fi
+            exec "$CARGO" build "$@"
+          '';
+        };
+      in {
+        packages.default = buildScript;
+
+        apps.default = {
+          type = "app";
+          program = "${buildScript}/bin/maisimai-build";
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+          ];
+
+          shellHook = commonEnv;
+        };
+      });
+}
