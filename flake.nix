73a7eecbcc6b06459feeba19d91b0c1a8680ad05
Neuron-Group <2062512253@qq.com>
2026-06-24 14:27:57 +0800

    Drop unsupported self.submodules input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "maisimai-rs";
 
   inputs = {
-    self.submodules = true;
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
